5a7b710baab5960bc4ef494fd14726d571e10740
Hayden <haydenbryant.student@careerdevs.com>
2022-03-28 19:54:59 -0400

    added create comments

--- src/PostList.js
+++ src/PostList.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { axios } from 'axios';
+import commentCreate from './commentCreate';
+import CommentCreate from './commentCreate';
 
 export default () => {
     const [posts, setPosts] = useState({});
@@ -23,6 +25,7 @@ export default () => {
             >
                 <div className='card-body'>
                     <h3>{post.title}</h3>
+                    <CommentCreate postId={post.id} />
                 </div>
             </div>
         )
